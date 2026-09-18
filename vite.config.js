3d9ebcb63ed480c6dad96ec7861222261a83b39d
Duc Manh <ndcm9125@gmail.com>
2026-09-18 12:59:58 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Dialectic-Artifacts-3D/', // Bắt buộc phải có 2 dấu gạch chéo trước và sau
 })
